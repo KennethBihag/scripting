bb6ad130c66bb2ba33dd63a0e69f62157f31a861
KennethBihag <1618kbb@gmail.com>
2025-09-02 19:50:46 +0800

    Fully working transient + linearization sim

--- matlab/SPICErman/SPICErman.m
+++ matlab/SPICErman/SPICErman.m
@@ -1,50 +1,54 @@
 Vin=   5;
-r1=    1e3;
-C1=    1e-6;
-Is=    2.74e-13;
+R1=100;
+Is=    1e-12;
+N=     1;
 Vt=    0.02585;
-RC=    C1*r1;
-tStop= RC*5*2;
-steps= 100;
+C1=    1e-6;
+RC=    C1*R1;
+tStop= RC*5*1.5;
+steps= 1000;
 dt=    tStop/steps;
 xPts=  linspace(0, tStop, steps+1);
-yPts=  zeros(1, steps+1);
+yPts=  zeros(3, steps+1);
 
 rc=dt/C1;
-n=1;
 Vc=0;
-for t=xPts      
+Vds=zeros(1,numel(xPts));
+for n=1:1:numel(xPts)
     Vd=0.7;
     dv=1;
     m=0;
     while dv > 0.0001
         m=m+1;
-        rd=Vt/(Is * exp(Vd/Vt));
-        ID=Is*(exp(Vd/Vt)-1);
+        rd=Vt/(Is * exp(Vd/(N*Vt)));
+        ID=Is*(exp(Vd/(N*Vt))-1);
         id=ID - Vd/rd;
 
         A=[
-            1, -r1;
-            1, rd
+            rd+R1+rc, rc;
+            1, 1;
         ];
         B=[
-            0;
-            id*rd + Vin
+            id*rd + Vin;
+            Vc/rc;
         ];
-        x=A\B;% V1, ir1
-        
+        x = A\B; % iv1 ic
         vdOld=Vd;
-        Vd=Vin-x(1);
-        dv=abs(Vd-vdOld);        
+        Vd=rd*(x(1)-id);
+        dv=abs(Vd-vdOld);
     end
-    fprintf('\t%d iters for linearization\n', m);
-    rp=r1*rc/(r1+rc);
-    currS=x(1)/rp;
-    ic = Vc/rc;
-    irc=(ic+currS)*(1/rc)/(1/r1+1/rc);
-    yPts(n)=Vc;
-    Vc=irc*rc;
-
-    n=n+1;
+    Vds(n)=Vd;
+    % fprintf('\t%d iters for linearization\n', m);
+    yPts(1, n)=x(1)*R1;% R1 voltage drop
+    yPts(2, n)=x(1);% Total current
+    yPts(3, n)=Vd; % Diode voltage drop
+    dVc=Vc-x(2)*rc;
+    Vc=Vc+dVc;
 end
-fprintf('%d iters for integration\n', n-1);
+% fprintf('%d iters for integration\n', n-1);
+data=[
+    xPts;
+    yPts(1,:);
+    yPts(2,:);
+    yPts(3,:);
+    yPts(1,:) .* yPts(2,:)];
